73a016c8e77837217f519c7b44ac611864509ec1
Nizar Khalife Iglesias <khalifenizar@gmail.com>
2017-06-01 08:26:12 -0400

    Add array loops example code

---
+++ example.js
@@ -0,0 +1,37 @@
+var arrayNames = ["Pedro", "Jake", "Joan", "Ariel", "Kevin", "Alia", "Marisa", "Nizar" ];
+  //                 0        1       2       3        4        5       6         7
+
+
+console.log('forEach loop (best for arrays)');
+console.log('------------------------------');
+console.log("");
+
+arrayNames.forEach(function (oneName, i) {
+  console.log( i );
+  console.log( oneName );
+
+  console.log( oneName.split("") );
+  console.log( oneName.toUpperCase() );
+  console.log( oneName.length );
+
+  console.log("");
+});
+
+console.log("");
+console.log("");
+
+
+console.log('for loop (works for arrays but not the best)');
+console.log('--------------------------------------------');
+console.log("");
+
+for (var i = 0; i < arrayNames.length; i += 1) {
+  console.log(i);
+  console.log( arrayNames[i] );
+
+  console.log( arrayNames[i].split("") );
+  console.log( arrayNames[i].toUpperCase() );
+  console.log( arrayNames[i].length );
+
+  console.log("");
+}
